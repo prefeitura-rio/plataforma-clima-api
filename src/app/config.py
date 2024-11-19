958be4c066b4335e09e1ef66a0a135e9cb8a6729
patriciacatandi <patriciabcatandi@gmail.com>
2024-11-19 16:10:26 -0300

    changing time horizon inside config.py

--- src/app/config.py
+++ src/app/config.py
@@ -201,12 +201,12 @@ RIONOWCAST_PRODUCTS_MAPPING = {
     RionowcastModelProductEnum.V1: {
         "column": "v1",
         "gcs_prefix": "v1",
-        "time_horizon": ["1", "2", "3"],
+        "time_horizon": ["1h", "2h", "3h"],
     },
     RionowcastModelProductEnum.V2: {
         "column": "v2",
         "gcs_prefix": "v2",
-        "time_horizon": ["1", "2", "3"],
+        "time_horizon": ["1h", "2h", "3h"],
     },
 }
 SENTRY_ENABLE = getenv_or_action("SENTRY_ENABLE", default="false").lower() == "true"
